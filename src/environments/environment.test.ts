ace632ac2ce0d106398f230aa78030d8ea64b7bc
皮落痕 <1012106967@qq.com>
2020-01-11 10:01:40 +0800

    Added：增加logo，增加paths映射

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,5 +3,5 @@ const address = window.location.hostname;
 export const environment = {
   production: true,
   LOGIN_URL: `http://${address}/iotPlant/#/`,
-  baseUrl: '/ipark-esp/'
+  baseUrl: '/'
 };
