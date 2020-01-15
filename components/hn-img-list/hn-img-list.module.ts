afdb445b42db2c16c226e651b26f09ab2ff91f2d
皮落痕 <1012106967@qq.com>
2020-01-15 16:37:58 +0800

    Publish：发布1.1.4版本，解决hn-image报错

--- components/hn-img-list/hn-img-list.module.ts
+++ components/hn-img-list/hn-img-list.module.ts
@@ -3,7 +3,7 @@ import { NgZorroModule } from '../ng-zorro.module';
 
 import { HnImgListComponent } from './hn-img-list.component';
 import { HnPreviewModule } from '../hn-preview/hn-preview.module';
-import { HnImageModule } from 'hn-ui/hn-image';
+import { HnImageModule } from '../hn-image/hn-image.module';
 
 @NgModule({
   imports: [NgZorroModule, HnPreviewModule, HnImageModule],
