01421eec35696c235283f9fc7df2ed3ae67b0bda
皮落痕 <1012106967@qq.com>
2020-03-17 23:00:39 +0800

    Debug：修复hn-search 按钮样式bug

--- components/hn-search/hn-search.module.ts
+++ components/hn-search/hn-search.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { NzIconModule, NzInputModule } from 'ng-zorro-antd';
+import { NzIconModule, NzInputModule, NzButtonModule } from 'ng-zorro-antd';
 import { HnFormModule } from 'hn-ui/hn-form';
 import { HnSearchComponent } from './hn-search.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, NzIconModule, NzInputModule, HnFormModule],
+  imports: [CommonModule, FormsModule, NzIconModule, NzInputModule, HnFormModule, NzButtonModule],
   declarations: [HnSearchComponent],
   exports: [HnSearchComponent]
 })
